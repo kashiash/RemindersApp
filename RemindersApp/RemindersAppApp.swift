419934b5751cb984cecd5a9dca901474a7bbbe52
Jacek Kosinski <kashiash@gmail.com>
2023-03-28 21:37:31 +0200

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Jacek Kosiński G on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
